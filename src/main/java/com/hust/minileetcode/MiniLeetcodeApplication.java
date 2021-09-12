70949403d409fcbdad6a1e77f253e57d324e5867
matdieuhau999 <matdieuhau999@gmail.com>
2021-09-12 15:04:21 +0700

    first commit

---
+++ src/main/java/com/hust/minileetcode/MiniLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.hust.minileetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniLeetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hust/minileetcode/MiniLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.minileetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
