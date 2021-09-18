dd973fed84976072148db3d13f8b49713dbac95f
matdieuhau999 <matdieuhau999@gmail.com>
2021-09-18 11:14:51 +0700

    add config spring web security for user login

--- src/main/java/com/hust/minileetcode/MiniLeetcodeApplication.java
+++ src/main/java/com/hust/minileetcode/MiniLeetcodeApplication.java
@@ -2,7 +2,11 @@ package com.hust.minileetcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Arrays;
+
+@EnableScheduling
 @SpringBootApplication
 public class MiniLeetcodeApplication {
 
---
+++ src/main/java/com/hust/minileetcode/config/RestConfig.java
@@ -0,0 +1,18 @@
+package com.hust.minileetcode.config;
+
+
+import com.hust.minileetcode.rest.user.DPerson;
+import com.hust.minileetcode.rest.user.DPersonUserLogin;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestConfig implements RepositoryRestConfigurer {
+
+//    @Override
+//    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration restConfig) {
+//        restConfig.exposeIdsFor(DPerson.class);
+//        restConfig.exposeIdsFor(DPersonUserLogin.class);
+//    }
+}
