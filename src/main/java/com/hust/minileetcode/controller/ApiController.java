9f4ba90435c0eafbc65f25ec4b1da092fc250b17
matdieuhau999 <matdieuhau999@gmail.com>
2021-09-18 11:30:22 +0700

    update config spring web security for user login

--- src/main/java/com/hust/minileetcode/controller/ApiController.java
+++ src/main/java/com/hust/minileetcode/controller/ApiController.java
@@ -1,28 +1,28 @@
 package com.hust.minileetcode.controller;
 
-//import lombok.AllArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.annotation.CurrentSecurityContext;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.http.HttpHeaders;
-//import java.util.HashMap;
-//import java.util.Map;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import java.util.HashMap;
+import java.util.Map;
 
-//@RestController
-//@CrossOrigin
-//@AllArgsConstructor(onConstructor = @__(@Autowired))
+@RestController
+@CrossOrigin
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ApiController {
-//    @GetMapping("/")
-//    public ResponseEntity<Map> home(@CurrentSecurityContext(expression = "authentication.name") String name) {
-//        Map<String, String> response = new HashMap<>();
-//        HttpHeaders headers = new HttpHeaders();
-//
-//        response.put("user", name);
-//        headers.set("Access-Control-Expose-Headers", "X-Auth-Token");
-//
-//        return ResponseEntity.ok().headers(headers).body(response);
-//    }
+    @GetMapping("/")
+    public ResponseEntity<Map> home(@CurrentSecurityContext(expression = "authentication.name") String name) {
+        Map<String, String> response = new HashMap<>();
+        HttpHeaders headers = new HttpHeaders();
+
+        response.put("user", name);
+        headers.set("Access-Control-Expose-Headers", "X-Auth-Token");
+
+        return ResponseEntity.ok().headers(headers).body(response);
+    }
 }
