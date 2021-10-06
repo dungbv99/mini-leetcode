260c2e25a59c461f9aed419a588785af0e79bfa3
matdieuhau999 <matdieuhau999@gmail.com>
2021-10-06 09:38:24 +0700

    delete unused

--- FrontEnd/mini-leetcode-frontend/src/utils/MapUtils.js
+++
@@ -1,4 +0,0 @@
-export function parseLatLng(s) {
-  let array = s.split(`,`);
-  return [parseFloat(array[0]), parseFloat(array[1])];
-}
--- FrontEnd/mini-leetcode-frontend/src/utils/NumberFormat.js
+++
@@ -1,6 +0,0 @@
-export function currencyFormat(number) {
-  return new Intl.NumberFormat("de-DE", {
-    style: "currency",
-    currency: "VND",
-  }).format(number);
-}
